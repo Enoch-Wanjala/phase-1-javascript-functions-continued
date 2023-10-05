c7f315e127d79b847582606aef00abc728e78d19
Enock <enochwanjala.97@gmail.com>
2023-10-05 20:57:27 +0300

    Completed function continued assignements

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(whatToDo="roller-skate"){
+    return `This Saturday, I want to ${whatToDo}!`
+}
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}mondayWork(`work from home`)
+
+
+function wrapAdjective(sign){
+    return function(){
+        if(sign==="||"){
+            return `You are ${sign}a dedicated programmer${sign}!`
+        }else if(sign==='*'){
+            return `You are ${sign}a hard worker${sign}!`
+        }
+    }
+}
